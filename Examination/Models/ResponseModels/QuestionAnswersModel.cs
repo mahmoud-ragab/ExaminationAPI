a89d34c4e0e0d1597f1d1a87daa06d6a5aa637ba
mahmoud.ragab@cis.asu.edu.eg <mahmo@DESKTOP-FHC66HL>
2019-12-02 22:46:29 +0200

    -add api models -add question api, service and repo

---
+++ Examination/Models/ResponseModels/QuestionAnswersModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Examination.Models
+{
+    public class QuestionAnswersModel
+    {
+        public QuestionModel Question { get; set; }
+        public List<AnswerModel> Answers { get; set; }
+    }
+}
---
+++ Service/QuestionService.cs
@@ -0,0 +1,20 @@
+﻿using Data;
+using Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class QuestionService
+    {
+        public QuestionRepository questionRepository = new QuestionRepository();
+
+        public List<Question> GetAll()
+        {
+            return questionRepository.GetAll();
+        } 
+    }
+}
