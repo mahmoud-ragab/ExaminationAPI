f6784236444abbafa0578bd52327528a27059a82
islamtareq0012 <islam_elmasry@hotmail.com>
2019-12-09 15:46:32 +0000

    generate exam

--- Data/Migrations/Configuration.cs
+++ Data/Migrations/Configuration.cs
@@ -11,7 +11,7 @@ namespace Data.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
             AutomaticMigrationDataLossAllowed = true;
         }
 
