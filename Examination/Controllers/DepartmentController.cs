4768a9148d761a991bf601ccf043269c0c9b7cb7
mahmoud.ragab@cis.asu.edu.eg <mahmo@DESKTOP-FHC66HL>
2019-12-14 23:47:51 +0200

    add department to register

---
+++ Examination/Controllers/DepartmentController.cs
@@ -0,0 +1,29 @@
+﻿using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Examination.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        public DepartmentService departmentService = new DepartmentService();
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            try
+            {
+                var depts = departmentService.GetAll().Select(d => new { Id = d.Id, Name = d.Name });
+                return Ok(depts);
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
