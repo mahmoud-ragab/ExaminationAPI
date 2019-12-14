e9d74b8adeb6e56be0b73757bf57e0ac5c9f3591
AmrAyoub97 <AmrAyoub97@gmail.com>
2019-12-15 00:30:07 +0200

    add instructor repo

--- Data/Repositories/InstructorRepository.cs
+++ Data/Repositories/InstructorRepository.cs
@@ -17,5 +17,25 @@ namespace Data.Repositories
             var instructor = context.Database.SqlQuery<Instructor>("getInstructorByID @ins_ID", _Ins_id).SingleOrDefault();
             return instructor;
         }
+        public Instructor getInstructorCourses(int id)
+        {
+            var _Ins_id = new SqlParameter("@ins_ID", id);
+            var courses = context.Database.SqlQuery<List<Course>>("getInstructorListOfCourses @ins_ID", _Ins_id).SingleOrDefault();
+            return courses;
+        }
+        public Instructor getInstructorExamList(int id,int c_id)
+        {
+            var _Ins_id = new SqlParameter("@ins_ID", id);
+            var _c_id = new SqlParameter("@c_ID", c_id);
+            var instructor = context.Database.SqlQuery<List<Exam>>("getInstructorByID @ins_ID @c_ID", _Ins_id,_c_id).SingleOrDefault();
+            return instructor;
+        }
+        public Instructor getInstructorInformation(int id)
+        {
+            var _Ins_id = new SqlParameter("@ins_ID", id);
+            var instructor = context.Database.SqlQuery<Instructor>("getInstructorByID @ins_ID", _Ins_id).SingleOrDefault();
+            return instructor;
+        }
+
     }
 }
