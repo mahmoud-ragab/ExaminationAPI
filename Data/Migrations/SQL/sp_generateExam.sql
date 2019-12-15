931f9dc18a47543a327b4784884147179c0d662f
islamtareq0012 <islam_elmasry@hotmail.com>
2019-12-15 22:10:25 +0200

    commit final changes

--- Data/Migrations/SQL/sp_generateExam.sql
+++ Data/Migrations/SQL/sp_generateExam.sql
@@ -1,4 +1,4 @@
-ALTER PROC genreateExam @courseID INT, @numberOfMcq INT, @numberOfTrue_False INT, @instructorID INT
+create PROC genreateExam @courseID INT, @numberOfMcq INT, @numberOfTrue_False INT, @instructorID INT
 AS
 BEGIN
 	DECLARE @numberOfMCQExist INT , @numberOfTrue_FalseExist int;
@@ -18,19 +18,26 @@ BEGIN
 					INSERT INTO Exam(Course_Id,Instructor_Id) values(@courseID , @instructorID)
 					declare @examID int = @@identity
 					UPDATE Question 
-					SET Exam_Id = @examID 
+					SET Exam_Id = @examID,
+					CoursedId = @courseID
 					WHERE Question.Id 
-					IN (SELECT  TOP(@numberOfMcq) Question.Id FROM Question where Question.questionType = 'MCQ' AND Question.Exam_Id IS NULL ORDER BY newid())
+					IN (SELECT  TOP(@numberOfMcq) Question.Id FROM Question where Question.questionType = 'MCQ' AND Question.Exam_Id IS NULL AND Question.CoursedId = @courseID ORDER BY newid())
 
 					UPDATE Question
-					SET Exam_Id = @examID 
+					SET Exam_Id = @examID,
+					CoursedId = @courseID
 					WHERE Question.Id 
-					IN (SELECT  TOP(@numberOfTrue_False) Question.Id FROM Question where Question.questionType = 'TRUE_FALSE' AND Question.Exam_Id IS NULL ORDER BY newid())
+					IN (SELECT  TOP(@numberOfTrue_False) Question.Id FROM Question where Question.questionType = 'TRUE_FALSE' AND Question.Exam_Id IS NULL AND Question.CoursedId = @courseID ORDER BY newid())
 				
 					SELECT * FROM Question WHERE Question.Exam_Id = @examID
 
 				END
+			ELSE
+				BEGIN
+					SELECT -1 AS 'Id' , 'N/A' AS 'Content' , -1 AS 'CorrectAnswer' , 'N/A' AS 'questionType' , -1 AS 'CoursedId' , -1 AS 'Exam_Id'
+				END
+		
 		END	
 END
 
---EXEC dbo.genreateExam 1,6,4 ,1
+EXEC dbo.genreateExam 1,6,4 ,1
