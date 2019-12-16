6e8234b1ab8c76af35243c51ef20944c4456a7c7
mahmoud.ragab@cis.asu.edu.eg <mahmo@DESKTOP-FHC66HL>
2019-12-16 10:42:54 +0200

    f

--- Examination/Controllers/InstructorController.cs
+++ Examination/Controllers/InstructorController.cs
@@ -29,18 +29,18 @@ namespace Examination.Controllers
             var res = exam.Select(e => new
             {
                 id = e.Id,
-                students = e.StudentExam.Where(ee=>ee.Exam_Id == id).Select(se=>new { se.Student.Id, se.Student.Name }).ToList()
-                //Students = e.Course.StudentCourse.Select(sc => new { sc.Student.Id, sc.Student.Name }).ToList()
+                //students = e.StudentExam.Where(ee=>ee.Exam_Id == id).Select(se=>new { se.Student.Id, se.Student.Name }).ToList()
+                students = e.Course.StudentCourse.Select(sc => new { sc.Student.Id, sc.Student.Name }).ToList()
             }).ToList();
             return Ok(res);
         }
         [Route("api/Instructor/{id}/course/{cid}/exam/{eid}/student/{sid}/modelanswer")]
-        public IHttpActionResult GetAnswerSheetListOfExam(int eid,int sid)
+        public IHttpActionResult GetAnswerSheetListOfExam(int eid, int sid)
         {
             var res = InstructorService.GetStudentAnswerSheet(eid, sid);
             var r = res.Select(ii => new
             {
-                Questions = new { questionContent = ii.Question.Content, correctAnswer = ii.Question.CorrectAnswer },
+                Questions = new { questionContent = ii.Question.Content, answers = ii.Question.Answers.Select(a => a.Content).ToList(), correctAnswer = ii.Question.CorrectAnswer },
                 selectedAnswer = new { asnwerContent = ii.Answer.Content, answerId = ii.Answer_Id }
             });
             return Ok(r);
